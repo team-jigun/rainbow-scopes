68d2d741412cd2079684726deb149624bfd55945
DokdoGalmaegi <sdh190323@sdh.hs.kr>
2021-03-19 17:36:29 +0900

    decoration

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const decorationType = vscode.window.createTextEditorDecorationType({
+  backgroundColor: 'green',
+  border: '2px solid white',
+})
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rainbow-scopes" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rainbow-scopes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Rainbow Scopes!');
-	});
-
-	context.subscriptions.push(disposable);
+  vscode.workspace.onWillSaveTextDocument(event => {
+    const openEditor = vscode.window.visibleTextEditors.filter(
+      editor => editor.document.uri === event.document.uri
+    )[0]
+    decorate(openEditor)
+  })
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function decorate(editor: vscode.TextEditor) {
+  let sourceCode = editor.document.getText()
+  let regex = /(console\.log)/
+
+  let decorationsArray: vscode.DecorationOptions[] = []
+
+  const sourceCodeArr = sourceCode.split('\n')
+
+  for (let line = 0; line < sourceCodeArr.length; line++) {
+    let match = sourceCodeArr[line].match(regex)
+
+    if (match !== null && match.index !== undefined) {
+      let range = new vscode.Range(
+        new vscode.Position(line, match.index),
+        new vscode.Position(line, match.index + match[1].length)
+      )
+
+      let decoration = { range }
+
+      decorationsArray.push(decoration)
+    }
+  }
+
+  editor.setDecorations(decorationType, decorationsArray)
+}
